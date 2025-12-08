14780916cc22f43e8602538fc53f1166f669687a
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-12-08 12:23:47 +0200

    add wp-content and database export

---
+++ wp-content/plugins/ultimate-blocks/dist/blocks.build.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '2bceea6b7b7506470522');
